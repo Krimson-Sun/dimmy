c9f79a66992bcaa664607f93b92979f1ccaa1f9d
firs-iln <firs_iln@mail.ru>
2021-09-23 19:23:08 +0300

    aaa

--- vyichislenie_vyrazheniy.py
+++ vyichislenie_vyrazheniy.py
@@ -1,6 +1,6 @@
-from PyQt5.QWidgets import QWidget, QApplication, QPushButton, QLabel, QLineEdit
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLabel, QLineEdit
+import PyQt5
 import sys
-
 
 
 class Example(QWidget):
@@ -9,7 +9,7 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 400, 400)
+        self.setGeometry(300, 300, 400, 200)
         self.setWindowTitle('Example')
 
         self.btn = QPushButton('Вычислить', self)
@@ -17,28 +17,27 @@ class Example(QWidget):
         self.btn.move(100, 150)
         self.btn.clicked.connect(self.count)
 
-        self.scr_label = QLabel(self)
+        self.src_label = QLabel(self)
         self.src_label.setText("Выражение:")
         self.src_label.move(40, 30)
 
         self.res_label = QLabel(self)
-        self.res_label.setText("Введите имя: ")
-        self.res_label.move(150, 30)
+        self.res_label.setText("Результат: ")
+        self.res_label.move(200, 30)
 
         self.src_input = QLineEdit(self)
         self.src_input.move(40, 60)
-        
+
         self.res_input = QLineEdit(self)
-        self.res_input.move(150, 60)
-        
+        self.res_input.move(200, 60)
+
     def count(self):
-        self.res_input.displayText(str(eval(self.src_input.text())))
-        
-        
-        
+        self.res_input.setText(str(eval(self.src_input.text())))
+        self.src_input.setText('')
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
     sys.exit(app.exec())
-        
