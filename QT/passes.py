d67136e03cecdc76b47020868b04c3dbbf2290f9
firs-iln <firs_iln@mail.ru>
2021-10-28 00:31:18 +0300

    initial commt

---
+++ QT/passes.py
@@ -0,0 +1,54 @@
+import PyQt5
+import os
+import sqlite3
+
+ADDRESS = 'C:/'
+AUTHED_USER_ID = 12345
+
+
+def auth(idd):
+    pass
+
+
+def read_tasks(idd):
+    pass
+
+
+def check_answers(answers):
+    pass
+
+
+class Database:
+    def __init__(self):
+        pass
+
+    def get_info(self):
+        pass
+
+    def del_info(self):
+        pass
+
+    def add_info(self):
+        pass
+
+
+class Teacher:
+    def __init__(self):
+        pass
+
+    def add_student(self):
+        pass
+
+    def del_student(self):
+        pass
+
+    def add_task(self):
+        pass
+
+
+class Student:
+    def __init__(self):
+        pass
+
+    def give_stats(self):
+        pass
